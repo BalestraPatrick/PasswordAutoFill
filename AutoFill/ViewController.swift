fc85f98a40ad16fc7c18b9f604cd555b1267b3aa
Patrick Balestra <me@patrickbalestra.com>
2017-06-23 23:05:39 +0200

    Initial Commit

---
+++ AutoFill/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoFill
+//
+//  Created by Patrick Balestra on 6/23/17.
+//  Copyright © 2017 Patrick Balestra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
